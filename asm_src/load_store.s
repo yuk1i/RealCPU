d83c32e1b00566351e0692a6def7935ef7ae3355
yuki <chenjf2020@mail.sustech.edu.cn>
2022-05-14 17:26:36 +0800

    use linker script and set start point to 0x1000

--- asm_src/load_store.s
+++ asm_src/load_store.s
@@ -1,8 +1,8 @@
 .data
    buf:   .word  0x00000055
-   w01:   .word  0x11111111
-   w02:   .word  0x22222222
-   w03:   .word  0x33333333
+   w01:   .word  0x12345678
+   w02:   .word  0x00000080
+   w03:   .word  0x00008000
    w04:   .word  0x44444444
    w05:   .word  0x55555555
    w06:   .word  0x66666666
@@ -14,4 +14,12 @@ main:
    lw $s0, buf
    sll $s1, $s0, 8
    or $s1, $s1, $s0
-   sw $s1, buf
+   sw $s1, buf       # 0x00005555
+   lb $t0, w02       # 0xFFFFFF80
+   lbu $t0, w02      # 0x00000080
+   lh $t0, w02       # 0x00000080
+
+   lh $t0, w03
+   lhu $t0, w03
+   la $a0, w03
+   lb $t0, 1($a0)
