f4f764c6eb316dd812c80270bcfd8a5d121593b7
Sch1Warz <105050125+Sch1Warz@users.noreply.github.com>
2022-05-18 11:07:36 +0800

    change system

--- asm_src/system.c
+++ asm_src/system.c
@@ -28,6 +28,12 @@ volatile int* uart_tx_fifo  = (int*) 0xFFFF0134;
 #define SEG7_CHAR_d ((unsigned int) 0b01111010)
 #define SEG7_CHAR_e ((unsigned int) 0b10011110)
 #define SEG7_CHAR_f ((unsigned int) 0b10001110)
+#define SEG7_CHAR_G ((unsigned int) 0b10111110)
+#define SEG7_CHAR_H ((unsigned int) 0b01101110)
+#define SEG7_CHAR_f ((unsigned int) 0b10001110)
+#define SEG7_CHAR_i ((unsigned int) 0b01100000)
+#define SEG7_CHAR_j ((unsigned int) 0b01110000)
+#define SEG7_CHAR_i ((unsigned int) 0b01100000)
 
 
 unsigned char seg7_hex_to_char[] = {SEG7_CHAR_0,SEG7_CHAR_1,SEG7_CHAR_2,SEG7_CHAR_3,
@@ -43,31 +49,228 @@ void display(unsigned int d) {
 }
 
 extern int cmain() {
-    register int counter = 0;
-    register int c2 = 0;
+    register int c;
+    unsigned int decide;
+    unsigned int a;
+    unsigned int b;
+    unsigned int highbit;
+    unsigned int is_palindrome = 1;
+    const unsigned int bit0 = 0b00000001;
+    const unsigned int bit1 = 0b00000010;
+    const unsigned int bit2 = 0b00000100;
+    const unsigned int bit3 = 0b00001000;
+    const unsigned int bit4 = 0b00010000;
+    const unsigned int bit5 = 0b00100000;
+    const unsigned int bit6 = 0b01000000;
+    const unsigned int bit7 = 0b10000000;
     while (1)
     {
-        // int rx_valid = *uart_rx_valid;
-        // int get = *uart_rx_fifo;
-        counter++;
-        if (!*uart_tx_busy) {
-            for(int i=0;i<8;i++){
-                *uart_tx_fifo = 'A' + i;
+        decide = (mmio_sw[23] << 2) | (mmio_sw[22] << 1) | mmio_sw[21];
+        a = (mmio_sw[15] << 7) | (mmio_sw[14] << 6) | (mmio_sw[13] << 5) | (mmio_sw[12] << 4) | (mmio_sw[11] << 3) | (mmio_sw[10] << 2) | (mmio_sw[9] << 1) | mmio_sw[8];
+        b = (mmio_sw[7] << 7) | (mmio_sw[6] << 6) | (mmio_sw[5] << 5) | (mmio_sw[4] << 4) | (mmio_sw[3] << 3) | (mmio_sw[2] << 2) | (mmio_sw[1] << 1) | mmio_sw[0];
+        is_palindrome = 1;
+        if(decide == 0){
+            for(int i = 0; i < 8; i++){
+                mmio_led[i] = mmio_sw[i + 8];
             }
-            *uart_tx_send = 1;
-            while(*uart_tx_busy) {
-                mmio_led[0] = *uart_tx_busy;
-                asm volatile("":::"memory");
+            for(int i = 15; i >= 8; i--){
+                c = mmio_sw[i];
+                if(c == 1){
+                    highbit = i;
+                    break;
+                }
             }
-            mmio_led[0] = *uart_tx_busy;
-            // *uart_tx_send = 1;
-            c2++;
+            for(int i = 8; i <= 8 + (highbit - 8) / 2; i++){
+                if(mmio_sw[i] != mmio_sw[highbit - (i - 8)]){
+                    is_palindrome = 0;
+                    break;
+                }
+            }  
+            if(is_palindrome == 1){
+                mmio_led[23] = 1;
+            }else{
+                mmio_led[23] = 0;
+            }
+        }else if(decide == 1){
+            mmio_led[23] = 0;
+            for(int i = 0; i < 16; i++){
+                c = mmio_sw[i];
+                mmio_led[i] = c;
+            }
+        }else if(decide == 2){
+            mmio_led[23] = 0;
+            for(int i = 0; i < 8; i++){
+                mmio_led[i] = mmio_sw[i + 8] & mmio_sw[i];
+            }
+        }else if(decide == 3){
+            mmio_led[23] = 0;
+            for(int i = 0; i < 8; i++){
+                c = mmio_sw[i + 8] | mmio_sw[i];
+                mmio_led[i] = c;
+            }
+        }else if(decide == 4){
+            mmio_led[23] = 0;
+            for(int i = 0; i < 8; i++){
+                c = mmio_sw[i + 8] ^ mmio_sw[i];
+                mmio_led[i] = c;
+            }
+        }else if(decide == 5){
+            mmio_led[23] = 0;
+            c = a << b;
+            int temp;
+            temp = c & bit0;
+            if(temp != 0){
+                mmio_led[0] = 1;
+            }else{
+                mmio_led[0] = 0;
+            }
+            temp = c & bit1;
+            if(temp != 0){
+                mmio_led[1] = 1;
+            }else{
+                mmio_led[1] = 0;
+            }
+            temp = c & bit2;
+            if(temp != 0){
+                mmio_led[2] = 1;
+            }else{
+                mmio_led[2] = 0;
+            }
+            temp = c & bit3;
+            if(temp != 0){
+                mmio_led[3] = 1;
+            }else{
+                mmio_led[3] = 0;
+            }
+            temp = c & bit4;
+            if(temp != 0){
+                mmio_led[4] = 1;
+            }else{
+                mmio_led[4] = 0;
+            }
+            temp = c & bit5;
+            if(temp != 0){
+                mmio_led[5] = 1;
+            }else{
+                mmio_led[5] = 0;
+            }
+            temp = c & bit6;
+            if(temp != 0){
+                mmio_led[6] = 1;
+            }else{
+                mmio_led[6] = 0;
+            }
+            temp = c & bit7;
+            if(temp != 0){
+                mmio_led[7] = 1;
+            }else{
+                mmio_led[7] = 0;
+            }
+        }else if(decide == 6){
+            mmio_led[23] = 0;
+            c = a >> b;
+            int temp;
+            temp = c & bit0;
+            if(temp != 0){
+                mmio_led[0] = 1;
+            }else{
+                mmio_led[0] = 0;
+            }
+            temp = c & bit1;
+            if(temp != 0){
+                mmio_led[1] = 1;
+            }else{
+                mmio_led[1] = 0;
+            }
+            temp = c & bit2;
+            if(temp != 0){
+                mmio_led[2] = 1;
+            }else{
+                mmio_led[2] = 0;
+            }
+            temp = c & bit3;
+            if(temp != 0){
+                mmio_led[3] = 1;
+            }else{
+                mmio_led[3] = 0;
+            }
+            temp = c & bit4;
+            if(temp != 0){
+                mmio_led[4] = 1;
+            }else{
+                mmio_led[4] = 0;
+            }
+            temp = c & bit5;
+            if(temp != 0){
+                mmio_led[5] = 1;
+            }else{
+                mmio_led[5] = 0;
+            }
+            temp = c & bit6;
+            if(temp != 0){
+                mmio_led[6] = 1;
+            }else{
+                mmio_led[6] = 0;
+            }
+            temp = c & bit7;
+            if(temp != 0){
+                mmio_led[7] = 1;
+            }else{
+                mmio_led[7] = 0;
+            }
+        }else if(decide == 7){
+            mmio_led[23] = 0;
+            c = ((int)a) >> b;
+            int temp;
+            temp = c & bit0;
+            if(temp != 0){
+                mmio_led[0] = 1;
+            }else{
+                mmio_led[0] = 0;
+            }
+            temp = c & bit1;
+            if(temp != 0){
+                mmio_led[1] = 1;
+            }else{
+                mmio_led[1] = 0;
+            }
+            temp = c & bit2;
+            if(temp != 0){
+                mmio_led[2] = 1;
+            }else{
+                mmio_led[2] = 0;
+            }
+            temp = c & bit3;
+            if(temp != 0){
+                mmio_led[3] = 1;
+            }else{
+                mmio_led[3] = 0;
+            }
+            temp = c & bit4;
+            if(temp != 0){
+                mmio_led[4] = 1;
+            }else{
+                mmio_led[4] = 0;
+            }
+            temp = c & bit5;
+            if(temp != 0){
+                mmio_led[5] = 1;
+            }else{
+                mmio_led[5] = 0;
+            }
+            temp = c & bit6;
+            if(temp != 0){
+                mmio_led[6] = 1;
+            }else{
+                mmio_led[6] = 0;
+            }
+            temp = c & bit7;
+            if(temp != 0){
+                mmio_led[7] = 1;
+            }else{
+                mmio_led[7] = 0;
+            }
         }
-        display(counter);
-        
-        // mmio_led[0] = *uart_rx_valid;
-        // mmio_led[1] = *uart_tx_busy;
-        // mmio_led[2] = *uart_tx_full;
-        // mmio_led[3] = *uart_tx_send;
+        }
+
     }
-}
