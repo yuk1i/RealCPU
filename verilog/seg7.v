ef6cf9bbec970a8856d28a168e2f1962a0c27e17
yuki <chenjf2020@mail.sustech.edu.cn>
2022-05-24 14:07:37 +0800

    fix seg7 fresh clk

--- verilog/seg7.v
+++ verilog/seg7.v
@@ -10,9 +10,19 @@ module seg7 (
 );
 
     reg [2:0] LED_activating_counter;
-    
+
+    reg [12:0] cnt;
+    wire div_clk = cnt[12] == 1;
 
     always @(posedge clk) begin
+        if (!rst_n) begin
+            cnt <= 0;
+        end else begin
+            cnt <= cnt + 1;
+        end
+    end    
+
+    always @(posedge div_clk) begin
         if(!rst_n) begin
             LED_activating_counter <= 0;
             LED_BITS <= 8'b0000_0001;
