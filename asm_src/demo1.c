5d0803db483dd0e2889a27c98107d3f2191d6cbd
Sch1Warz <105050125+Sch1Warz@users.noreply.github.com>
2022-05-24 11:22:21 +0800

    change demo1 and demo2

--- asm_src/demo1.c
+++ asm_src/demo1.c
@@ -24,23 +24,47 @@ volatile int* mmio_seg7 = (int*) 0xFFFF0100;
 #define SEG7_CHAR_f ((unsigned int) 0b10001110)
 #define SEG7_CHAR_G ((unsigned int) 0b10111110)
 #define SEG7_CHAR_H ((unsigned int) 0b01101110)
-#define SEG7_CHAR_f ((unsigned int) 0b10001110)
 #define SEG7_CHAR_i ((unsigned int) 0b01100000)
-#define SEG7_CHAR_j ((unsigned int) 0b01110000)
-#define SEG7_CHAR_i ((unsigned int) 0b01100000)
+#define SEG7_CHAR_j ((unsigned int) 0b01111000)
+#define SEG7_CHAR_k ((unsigned int) 0b01011110)
+#define SEG7_CHAR_L ((unsigned int) 0b00011100)
+#define SEG7_CHAR_m ((unsigned int) 0b11101100)
+#define SEG7_CHAR_n ((unsigned int) 0b00101010)
+#define SEG7_CHAR_o ((unsigned int) 0b11111100)
+#define SEG7_CHAR_p ((unsigned int) 0b11001110)
+#define SEG7_CHAR_q ((unsigned int) 0b11100110)
+#define SEG7_CHAR_r ((unsigned int) 0b10001100)
+#define SEG7_CHAR_s ((unsigned int) 0b10110110)
+#define SEG7_CHAR_t ((unsigned int) 0b11100000)
+#define SEG7_CHAR_u ((unsigned int) 0b01111100)
+#define SEG7_CHAR_v ((unsigned int) 0b00111000)
+#define SEG7_CHAR_w ((unsigned int) 0b01111110)
+#define SEG7_CHAR_x ((unsigned int) 0b00100110)
+#define SEG7_CHAR_y ((unsigned int) 0b01110110)
+#define SEG7_CHAR_z ((unsigned int) 0b11011010)
 
+// unsigned char seg7_hex_to_char[] = {SEG7_CHAR_0,SEG7_CHAR_1,SEG7_CHAR_2,SEG7_CHAR_3,
+//                                     SEG7_CHAR_4,SEG7_CHAR_5,SEG7_CHAR_6,SEG7_CHAR_7,
+//                                     SEG7_CHAR_8,SEG7_CHAR_9,SEG7_CHAR_A,SEG7_CHAR_b,
+//                                     SEG7_CHAR_c,SEG7_CHAR_d,SEG7_CHAR_e,SEG7_CHAR_f};
 
-unsigned char seg7_hex_to_char[] = {SEG7_CHAR_0,SEG7_CHAR_1,SEG7_CHAR_2,SEG7_CHAR_3,
-                                    SEG7_CHAR_4,SEG7_CHAR_5,SEG7_CHAR_6,SEG7_CHAR_7,
-                                    SEG7_CHAR_8,SEG7_CHAR_9,SEG7_CHAR_A,SEG7_CHAR_b,
-                                    SEG7_CHAR_c,SEG7_CHAR_d,SEG7_CHAR_e,SEG7_CHAR_f};
+unsigned int seg7_char_int[] = {SEG7_CHAR_0, SEG7_CHAR_1, SEG7_CHAR_2, SEG7_CHAR_3, SEG7_CHAR_4, SEG7_CHAR_5, SEG7_CHAR_6, SEG7_CHAR_7, SEG7_CHAR_8, SEG7_CHAR_9};
+unsigned int seg7_char_alpha[] = {SEG7_CHAR_A, SEG7_CHAR_b, SEG7_CHAR_c, SEG7_CHAR_d, SEG7_CHAR_e, SEG7_CHAR_f, SEG7_CHAR_G, SEG7_CHAR_H, SEG7_CHAR_i, SEG7_CHAR_j, SEG7_CHAR_k, SEG7_CHAR_L, SEG7_CHAR_m, SEG7_CHAR_n, SEG7_CHAR_o, SEG7_CHAR_p, SEG7_CHAR_q, SEG7_CHAR_r, SEG7_CHAR_s, SEG7_CHAR_t, SEG7_CHAR_u, SEG7_CHAR_v, SEG7_CHAR_w, SEG7_CHAR_x, SEG7_CHAR_y, SEG7_CHAR_z};
 
-void display(unsigned int d) {
-    for(int i=0;i<8;i++) {
-        mmio_seg7[i] = seg7_hex_to_char[d & 0xF];
-        d = d >> 4;
-    }
+unsigned int get_seg7_char(char a) {
+    // [0-9][a-z][A-Z]
+    if (a >= '0' && a <= '9') return seg7_char_int[a - '0'];
+    if (a >= 'a' && a <= 'z') return seg7_char_alpha[a - 'a'];
+    if (a >= 'A' && a <= 'Z') return seg7_char_alpha[a - 'A'];
+    return (unsigned int) 0b00000001;
 }
+
+// void display(unsigned int d) {
+//     for(int i=0;i<8;i++) {
+//         mmio_seg7[i] = seg7_hex_to_char[d & 0xF];
+//         d = d >> 4;
+//     }
+// }
 int bit[8] = {0b00000001, 0b00000010, 0b00000100, 0b00001000, 0b00010000, 0b00100000, 0b01000000, 0b10000000};
 
 extern int main() {
@@ -55,8 +79,7 @@ extern int main() {
     unsigned int last_decide = 0;
     while (1)
     {
-        if (decide != 0)
-            mmio_led[23] = 0;
+        
         mmio_led[22] = mmio_sw[23];
         mmio_led[21] = mmio_sw[22];
         mmio_led[20] = mmio_sw[21];
@@ -71,6 +94,10 @@ extern int main() {
         a = (mmio_sw[15] << 7) | (mmio_sw[14] << 6) | (mmio_sw[13] << 5) | (mmio_sw[12] << 4) | (mmio_sw[11] << 3) | (mmio_sw[10] << 2) | (mmio_sw[9] << 1) | mmio_sw[8];
         b = (mmio_sw[7] << 7) | (mmio_sw[6] << 6) | (mmio_sw[5] << 5) | (mmio_sw[4] << 4) | (mmio_sw[3] << 3) | (mmio_sw[2] << 2) | (mmio_sw[1] << 1) | mmio_sw[0];
         is_palindrome = 1;
+
+        if (decide != 0){
+            mmio_led[23] = 0;
+        }
         if(decide == 0){
             for(int i = 0; i < 8; i++){
                 mmio_led[i] = mmio_sw[i + 8];
@@ -91,32 +118,39 @@ extern int main() {
             }
             mmio_led[23] = is_palindrome;
         }else if(decide == 1){
+            mmio_led[23] = 0;
             for(int i = 0; i < 16; i++) {
                 mmio_led[i] = mmio_sw[i];
             }
         }else if(decide == 2){
+            mmio_led[23] = 0;
             for(int i = 0; i < 8; i++) {
                 mmio_led[i] = mmio_sw[i + 8] & mmio_sw[i];
             }
         }else if(decide == 3){
+            mmio_led[23] = 0;
             for(int i = 0; i < 8; i++) {
                 mmio_led[i] = mmio_sw[i + 8] | mmio_sw[i];
             }
         }else if(decide == 4){
+            mmio_led[23] = 0;
             for(int i = 0; i < 8; i++){
                 mmio_led[i] = mmio_sw[i + 8] ^ mmio_sw[i];
             }
         }else if(decide == 5){
+            mmio_led[23] = 0;
             c = a << b;
             for(int i = 0; i < 8; i++){
                 mmio_led[i] = (bit[i] & c) != 0;
             }
         }else if(decide == 6){
+            mmio_led[23] = 0;
             c = a >> b;
             for(int i = 0; i < 8; i++){
                 mmio_led[i] = (bit[i] & c) != 0;
             }
         }else if(decide == 7){
+            mmio_led[23] = 0;
             signed char sa = a & 0xFF;
             signed char sc = sa >> b;
             for(int i = 0; i < 8; i++){
--- asm_src/demo2.c
+++ asm_src/demo2.c
@@ -21,20 +21,36 @@ volatile int* mmio_seg7 = (int*) 0xFFFF0100;
 #define SEG7_CHAR_f ((unsigned int) 0b10001110)
 #define SEG7_CHAR_G ((unsigned int) 0b10111110)
 #define SEG7_CHAR_H ((unsigned int) 0b01101110)
-#define SEG7_CHAR_f ((unsigned int) 0b10001110)
 #define SEG7_CHAR_i ((unsigned int) 0b01100000)
-#define SEG7_CHAR_j ((unsigned int) 0b01110000)
-#define SEG7_CHAR_i ((unsigned int) 0b01100000)
+#define SEG7_CHAR_j ((unsigned int) 0b01111000)
+#define SEG7_CHAR_k ((unsigned int) 0b01011110)
+#define SEG7_CHAR_L ((unsigned int) 0b00011100)
+#define SEG7_CHAR_m ((unsigned int) 0b11101100)
+#define SEG7_CHAR_n ((unsigned int) 0b00101010)
+#define SEG7_CHAR_o ((unsigned int) 0b11111100)
+#define SEG7_CHAR_p ((unsigned int) 0b11001110)
+#define SEG7_CHAR_q ((unsigned int) 0b11100110)
+#define SEG7_CHAR_r ((unsigned int) 0b10001100)
+#define SEG7_CHAR_s ((unsigned int) 0b10110110)
+#define SEG7_CHAR_t ((unsigned int) 0b11100000)
+#define SEG7_CHAR_u ((unsigned int) 0b01111100)
+#define SEG7_CHAR_v ((unsigned int) 0b00111000)
+#define SEG7_CHAR_w ((unsigned int) 0b01111110)
+#define SEG7_CHAR_x ((unsigned int) 0b00100110)
+#define SEG7_CHAR_y ((unsigned int) 0b01110110)
+#define SEG7_CHAR_z ((unsigned int) 0b11011010)
 
-unsigned int seg7_char_int[] = {SEG7_CHAR_0, SEG7_CHAR_1, SEG7_CHAR_2};
-unsigned int seg7_char_alpha[] = {SEG7_CHAR_A, SEG7_CHAR_b, SEG7_CHAR_c};
+unsigned int seg7_char_int[] = {SEG7_CHAR_0, SEG7_CHAR_1, SEG7_CHAR_2, SEG7_CHAR_3, SEG7_CHAR_4, SEG7_CHAR_5, SEG7_CHAR_6, SEG7_CHAR_7, SEG7_CHAR_8, SEG7_CHAR_9};
+unsigned int seg7_char_alpha[] = {SEG7_CHAR_A, SEG7_CHAR_b, SEG7_CHAR_c, SEG7_CHAR_d, SEG7_CHAR_e, SEG7_CHAR_f, SEG7_CHAR_G, SEG7_CHAR_H, SEG7_CHAR_i, SEG7_CHAR_j, SEG7_CHAR_k, SEG7_CHAR_L, SEG7_CHAR_m, SEG7_CHAR_n, SEG7_CHAR_o, SEG7_CHAR_p, SEG7_CHAR_q, SEG7_CHAR_r, SEG7_CHAR_s, SEG7_CHAR_t, SEG7_CHAR_u, SEG7_CHAR_v, SEG7_CHAR_w, SEG7_CHAR_x, SEG7_CHAR_y, SEG7_CHAR_z};
 
 unsigned int get_seg7_char(char a) {
     // [0-9][a-z][A-Z]
-    if (a>='0' && a<='9') return seg7_char_int[a-'0'];
-    if (a>='a' && a <= 'z') return seg7_char_alpha[a-'a'];
+    if (a >= '0' && a <= '9') return seg7_char_int[a - '0'];
+    if (a >= 'a' && a <= 'z') return seg7_char_alpha[a - 'a'];
+    if (a >= 'A' && a <= 'Z') return seg7_char_alpha[a - 'A'];
     return (unsigned int) 0b00000001;
 }
+
 int data0[257];
 int data1[257];
 int data2[257];
@@ -44,7 +60,7 @@ int num1 = 0;
 int num2 = 0;
 int num3 = 0;
 int c = 0;
-int needtime = 16666666665;
+int needtime = 3333333333;
 int bit[8] = {0b00000001, 0b00000010, 0b00000100, 0b00001000, 0b00010000, 0b00100000, 0b01000000, 0b10000000};
 extern int main() {
     unsigned int decide;
@@ -56,21 +72,14 @@ extern int main() {
     int joint;
     unsigned int dataset;
     unsigned int index;
-    const unsigned int bit0 = 0b00000001;
-    const unsigned int bit1 = 0b00000010;
-    const unsigned int bit2 = 0b00000100;
-    const unsigned int bit3 = 0b00001000;
-    const unsigned int bit4 = 0b00010000;
-    const unsigned int bit5 = 0b00100000;
-    const unsigned int bit6 = 0b01000000;
-    const unsigned int bit7 = 0b10000000;
+    int complement;
     while (1)
     {
         decide = (mmio_sw[23] << 2) | (mmio_sw[22] << 1) | mmio_sw[21];
         a = (mmio_sw[7] << 7) | (mmio_sw[6] << 6) | (mmio_sw[5] << 5) | (mmio_sw[4] << 4) | (mmio_sw[3] << 3) | (mmio_sw[2] << 2) | (mmio_sw[1] << 1) | mmio_sw[0];
-        //_sleep(5000);
         dataset = (mmio_sw[20] << 1) | mmio_sw[19];
         index = (mmio_sw[18] << 3) | (mmio_sw[17] << 2) | (mmio_sw[16] << 1) | mmio_sw[15];
+        complement = -(mmio_sw[7] << 7) + (mmio_sw[6] << 6) + (mmio_sw[5] << 5) + (mmio_sw[4] << 4) + (mmio_sw[3] << 3) + (mmio_sw[2] << 2) + (mmio_sw[1] << 1) + mmio_sw[0];
         if(decide == 0){
             c = 0;
             for(int i = 0; i < num0; i++){
@@ -106,24 +115,13 @@ extern int main() {
             num2 = num0;
             for(int i = 0; i < num0; i++){
                 c = data0[i];
-                temp = c & bit7;
+                temp = c & (1 << 7);
                 if(temp == 0){
                     data2[i] = c;
                 }else{
-                    temp = c & bit0;
-                    joint += temp;
-                    temp = c & bit1;
-                    joint += temp;
-                    temp = c & bit2;
-                    joint += temp;
-                    temp = c & bit3;
-                    joint += temp;
-                    temp = c & bit4;
-                    joint += temp;
-                    temp = c & bit5;
-                    joint += temp;
-                    temp = c & bit6;
-                    joint += temp;
+                    for(int i = 0; i < 7; i++){
+                        joint += c & (1 << i);
+                    }
                     data2[i] = -joint;
                 }
             }
@@ -147,20 +145,15 @@ extern int main() {
             }
         }else if(decide == 4){
             if(dataset == 1){
-                c = data1[num1] - data1[0];
+                c = data1[num1 - 1] - data1[0];
             }else if(dataset == 3){
-                c = data3[num3] - data3[0];
+                c = data3[num3 - 1] - data3[0];
             }else{
                 c = 0;
             }
 
             for(int i = 0; i < 8; i++){
-                temp = bit[i] & c;
-                if(temp != 0){
-                    mmio_led[i] = 1;
-                }else{
-                    mmio_led[i] = 0;
-                }
+                mmio_led[i] = (bit[i] & c) != 0;
             }
         
         }else if(decide == 5){
@@ -172,12 +165,7 @@ extern int main() {
                 c = 0;
             }
             for(int i = 0; i < 8; i++){
-                temp = bit[i] & c;
-                if(temp != 0){
-                    mmio_led[i] = 1;
-                }else{
-                    mmio_led[i] = 0;
-                }
+                mmio_led[i] = (bit[i] & c) != 0;
             }
         }else if(decide == 6){
             if(dataset == 1){
@@ -195,115 +183,54 @@ extern int main() {
                 mmio_led[8] = 1;
                 c = -c;
             }
-            temp = c & bit7;
-            if(temp != 0){
-                b = 7;
-            }else{
-                temp = c & bit6;
-                if(temp != 0){
-                    b = 6;
-                }else{
-                    temp = c & bit5;
-                    if(temp != 0){
-                    b = 5;
-                    }else{
-                        temp = c & bit4;
-                        if(temp != 0){
-                            b = 4;
-                        }else{
-                            temp = b & bit3;
-                            if(temp != 0){
-                                b = 3;
-                            }else{
-                                temp = b & bit2;
-                                if(temp != 0){
-                                    b = 2;
-                                }else{
-                                    temp = b & bit1;
-                                    if(temp != 0){
-                                        b = 1;
-                                    }else{
-                                        b = 0;
-                                    }
-                                }
-                            }
-                        }
-                    }
-                }
-            }
-            for(int i = 0; i < 8; i++){
-                temp = bit[i] & b;
-                if(temp != 0){
-                    mmio_led[i] = 1;
-                }else{
-                    mmio_led[i] = 0;
+            b = 0;
+            for(int i = 7; i >= 0; i--){
+                if((c & (1 << i)) != 0){
+                    b = i;
+                    break;
                 }
             }
             
-        }else if(decide == 7){
-            for(int i = 0; i < 16666666665; i++){
-                i = 0;
+            for(int i = 0; i < 8; i++){
+                mmio_led[i] = (bit[i] & b) != 0;
             }
+            
+        }else if(decide == 7){
+            for(int j = 0; j < 5; j++){
+                for(int i = 0; i < 3333333333; i++){
+                    i = 0;
+                }
+            }
+            
             c = data0[index];
             for(int i = 0; i < 8; i++){
-                temp = bit[i] & c;
-                if(temp != 0){
-                    mmio_led[i] = 1;
-                }else{
-                    mmio_led[i] = 0;
+                mmio_led[i] = (bit[i] & c) != 0;
+            }
+
+            for(int j = 0; j < 5; j++){
+                for(int i = 0; i < 3333333333; i++){
+                    i = 0;
                 }
             }
-
-            for(int i = 0; i < 16666666665; i++){
-                i = 0;
-            }
-
-            temp = c & bit7;
-            if(temp == 1){
+            if(complement > 0){
+                c = complement;
                 mmio_led[8] = 1;
             }else{
+                c = -complement;
                 mmio_led[8] = 0;
             }
-            temp = c & bit6;
-            if(temp != 0){
-                b = 6;
-            }else{
-                temp = c & bit5;
-                if(temp != 0){
-                    b = 5;
-                }else{
-                    temp = c & bit4;
-                    if(temp != 0){
-                        b = 4;
-                    }else{
-                        temp = b & bit3;
-                        if(temp != 0){
-                            b = 3;
-                        }else{
-                            temp = b & bit2;
-                            if(temp != 0){
-                                b = 2;
-                            }else{
-                                temp = b & bit1;
-                                if(temp != 0){
-                                    b = 1;
-                                }else{
-                                    b = 0;
-                                }
-                            }
-                        }
-                    }
+
+            b = 0;
+            for(int i = 7; i >= 0; i--){
+                if((c & (1 << i)) != 0){
+                    b = i;
+                    break;
                 }
             }
+
             for(int i = 0; i < 8; i++){
-                temp = bit[i] & b;
-                if(temp != 0){
-                    mmio_led[i] = 1;
-                }else{
-                    mmio_led[i] = 0;
-                }
+                mmio_led[i] = (bit[i] & b) != 0;
             }
         }
     }
-
 }
