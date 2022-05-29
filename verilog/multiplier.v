9f07fba3646f4c981a763a2c3f6df28a8d5abd2e
yuki <chenjf2020@mail.sustech.edu.cn>
2022-05-30 01:52:08 +0800

    update uart clk frequency

--- verilog/multiplier.v
+++ verilog/multiplier.v
@@ -1,24 +1,4 @@
 `timescale 1ns / 1ps
-//////////////////////////////////////////////////////////////////////////////////
-// Company: 
-// Engineer: 
-// 
-// Create Date: 2022/05/13 23:23:16
-// Design Name: 
-// Module Name: multiplexer
-// Project Name: 
-// Target Devices: 
-// Tool Versions: 
-// Description: 
-// 
-// Dependencies: 
-// 
-// Revision:
-// Revision 0.01 - File Created
-// Additional Comments:
-// 
-//////////////////////////////////////////////////////////////////////////////////
-
 
 module multiplier(
     input clk,
@@ -31,11 +11,10 @@ module multiplier(
     output done
 );
     
-    reg start;
     reg [2:0] counter;
     wire [63:0] re_unsign;
     wire [63:0] re_sign;
-    wire _done = counter == 6 ? 1'b1 : 1'b0;
+    wire _done = counter == 1 ? 1'b1 : 1'b0;
     reg _done_delay;
     always @(posedge clk) _done_delay <= _done;
 
@@ -43,42 +22,31 @@ module multiplier(
 
     // short for ifetch design 6 --> 5
     mult_sign uut(
-        .CLK(~clk),
+        .CLK(clk),
         .A(a),
         .B(b),
         .P(re_sign),
-        .CE(start)
+        .CE(enable)
     );
     
     mult_unsign neko(
-        .CLK(~clk),
+        .CLK(clk),
         .A(a),
         .B(b),
         .P(re_unsign),
-        .CE(start)
+        .CE(enable)
     );
     
     assign result = is_unsign ? re_unsign : re_sign;
-
     
     always @(posedge clk) begin
-        if (!rst_n) start = 0;
-        else if (!start && enable == 1 && !done)
-            start = 1;
-        else if (start && !done) start = 1;
-        else start = 0;
-    end
-    
-    always @(negedge clk) begin
         if (!rst_n) begin
             counter <= 3'b0;
         end else begin
-            if (start) begin
-                if (done)
-                    counter <= 0;
-                else 
-                    counter <= counter + 1;
-            end else counter <= 0;
+            if (enable) begin
+                counter <= counter + 1;
+            end 
+                else counter <= 0;
         end
     end
 endmodule
