cb8c097b1f3aeeb8a876803d73253d23fcbd74c3
Sch1Warz <105050125+Sch1Warz@users.noreply.github.com>
2022-05-30 20:15:52 +0800

    jiejie

--- asm_src/io.h
+++ asm_src/io.h
@@ -4,3 +4,4 @@
 unsigned int read_io_u32(unsigned int address);
 void write_io_u32(unsigned int address, unsigned int value);
 
+void delay_1ms();
--- asm_src/io.s
+++ asm_src/io.s
@@ -12,3 +12,18 @@ write_io_u32:
     sw $a1, ($a0)
     nop
     jr $ra 
+
+.globl delay_1ms
+delay_1ms:
+    addiu $sp, $sp, -8
+    sw $a0, 4($sp)
+    sw $a1, 8($sp)
+    li $a1, 10000
+    li $a0, 0
+loop:
+    addiu $a0, $a0, 1
+    bne $a0, $a1, loop
+    lw $a0, 4($sp)
+    lw $a1, 8($sp)
+    jr $ra
+
